14329b368a0c583c264b2194bfc7959d72af1736
Ivan Villacorta <ivan_mv23@hotmail.com>
2024-10-21 19:26:44 -0300

    Exercicios usando .times do Lodash

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,5 +1,7 @@
-it('Testa a página da politica de privacidade de forma independente', function() {
-    cy.visit('./src/privacy.html')
-
-    cy.contains('Talking About Testing').should('be.visible')
-})  
+Cypress._.times(3, () => {
+    it('Testa a página da politica de privacidade de forma independente', function() {
+        cy.visit('./src/privacy.html')
+    
+        cy.contains('Talking About Testing').should('be.visible')
+    })  
+})
